1339a365111c539fbacd1e9bb26aff10d700d7cb
zhengwei <zw30522@live.com>
2021-12-29 10:39:37 +0800

    修改浏览器驱动包参数

--- spider/selenium1.py
+++ spider/selenium1.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 # 事先将 Edge 的驱动包放到和 python 同级目录
-web = Edge()
+web = Edge('./msedgedriver.exe')
 web.get('http://www.baidu.com')
 web.find_element(by=By.XPATH, value='//*[@id="kw"]').send_keys('666', Keys.ENTER)
 # el = web.find_element(by=By.XPATH, value='//*[@id="su"]')
